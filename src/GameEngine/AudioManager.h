a23e91eb4ced57e24fb83722b9d1f7d9bb4f1e83
TimuSumisu <tesmith@gmail.com>
2013-11-29 20:10:50 -0500

    updated audio with several sound effects. using audio.playSound(int) will yield
    1 - menu hoveer
    2 - menu click
    3 - sword attack
    4 - bow attack
    5 - fanfare
    
    audio.playMusic(int) will provide
    1 - title music
    2 - menu music
    
    more will be added

---
+++ src/GameEngine/AudioManager.h
@@ -0,0 +1,49 @@
+/*
+ * AudioManager.h
+ *
+ *      Author: tim
+ */
+
+#ifndef AUDIOMANAGER_H_
+#define AUDIOMANAGER_H_
+
+#include "SDL_mixer.h"
+#include "SDL.h"
+#include <map>
+#include <string>
+
+
+//Mix_PlayChannel
+//Mix_PausedMusic
+//Mix_HaltMusic
+//Mix_PlayMusic
+//scratch = Mix_LoadWAV( "scratch.wav" );
+
+class AudioManager
+{
+    public:
+
+        static AudioManager * getInstance();
+
+        Mix_Music *music = NULL;
+        Mix_Chunk *sfx = NULL;
+      //  std::map Soundmap pair<>
+        //std::map<Mix_Music, int> musicMap;
+//        std::map<Mix_Chunk, int> soundMap;
+
+        int song;
+        void load_files();
+        void clean_up();
+        void init();
+        void run();
+        void playMusic(int);
+        void playSound(int);
+        void stop();
+
+    private:
+        ~AudioManager();
+        AudioManager();
+        static AudioManager * instance;
+};
+
+#endif /* AUDIOMANAGER_H_ */
